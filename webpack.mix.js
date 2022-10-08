e11381546906e0c811f6539eaa64bbf3b59a3d08
denischafer <denischafer@gmail.com>
2022-10-08 18:42:59 -0300

    Add Vue.JS

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js');
+mix.js('resources/js/app.js', 'public/js').vue();
 mix.styles(['resources/js/app.js'], 'public/css/app.css').version();
 
 mix.styles([
